9ed829afdc4208db90d45fb8958d8d1d892994f0
bartvanhoey <bartvanhoey@hotmail.com>
2021-01-21 17:49:42 +0100

    Update

--- IssueTracking/src/IssueTracking.Domain/Issues/IIssueRepository.cs
+++ IssueTracking/src/IssueTracking.Domain/Issues/IIssueRepository.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Volo.Abp.Domain.Repositories;
 
 namespace IssueTracking.Domain.Issues
 {
     public interface IIssueRepository :  IRepository<Issue, Guid>
     {
-         
+         Task<List<Issue>> GetInActiveIssuesAsync();
     }
 }
--- IssueTracking/src/IssueTracking.EntityFrameworkCore/Issues/EfCoreIssueRepository.cs
+++ IssueTracking/src/IssueTracking.EntityFrameworkCore/Issues/EfCoreIssueRepository.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using IssueTracking.Domain.Issues;
+using Microsoft.EntityFrameworkCore;
 using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -11,7 +14,28 @@ namespace IssueTracking.EntityFrameworkCore.Issues
     public EfCoreIssueRepository(IDbContextProvider<IssueTrackingDbContext> dbContextProvider) : base(dbContextProvider)
     {
     }
-    
+
+    public async Task<List<Issue>> GetInActiveIssuesAsync()
+    {
+
+      var daysAgo30 = DateTime.Now.Subtract(TimeSpan.FromDays(30));
+
+      return await DbSet.Where(i =>
+
+          //Open
+          !i.IsClosed &&
+
+          //Assigned to nobody
+          i.AssignedUserId == null &&
+
+          //Created 30+ days ago
+          i.CreationTime < daysAgo30 &&
+
+          //No comment or the last comment was 30+ days ago
+          (i.LastCommentTime == null || i.LastCommentTime < daysAgo30)
+
+      ).ToListAsync();
+    }
     public override IQueryable<Issue> WithDetails()
     {
       return GetQueryable().IncludeDetails(); // Uses the extension method defined above
