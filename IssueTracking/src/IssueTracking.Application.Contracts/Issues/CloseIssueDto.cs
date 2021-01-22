a71d167eaf6207e9c96d63dcfdeb51ff9d08886c
bartvanhoey <bartvanhoey@hotmail.com>
2021-01-22 21:29:58 +0100

    Update

---
+++ IssueTracking/src/IssueTracking.Application.Contracts/Issues/CloseIssueDto.cs
@@ -0,0 +1,12 @@
+using System;
+using IssueTracking.Domain.Shared.Issues;
+using Volo.Abp.Application.Dtos;
+
+namespace IssueTracking.Application.Contracts.Issues
+{
+  public class CloseIssueDto : EntityDto<Guid>
+  {
+    public IssueCloseReason CloseReason { get; set; }
+
+  }
+}
