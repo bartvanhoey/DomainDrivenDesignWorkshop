182d28a623208282c379f04a332606d9755341fc
bartvanhoey <bartvanhoey@hotmail.com>
2021-01-22 21:54:06 +0100

    Update

--- IssueTracking/src/IssueTracking.Application/IssueAppService.cs
+++ IssueTracking/src/IssueTracking.Application/IssueAppService.cs
@@ -74,30 +74,5 @@ namespace IssueTracking.Application
       issue.AddComment(CurrentUser.GetId(), input.Text);
       await _issueRepository.UpdateAsync(issue, autoSave: false);
     }
-
-    public async Task CloseAsync(Guid id, CloseIssueDto input)
-    {
-      var issue = await _issueRepository.GetAsync(id);
-      issue.Close(input.CloseReason);
-    }
-
-    public async Task ReOpenAsync(Guid id)
-    {
-      var issue = await _issueRepository.GetAsync(id);
-      issue.ReOpen();
-    }
-
-    public async Task LockAsync(Guid id)
-    {
-      var issue = await _issueRepository.GetAsync(id);
-      issue.Lock();
-
-    }
-
-    public async Task UnlockAsync(Guid id)
-    {
-      var issue = await _issueRepository.GetAsync(id);
-      issue.Unlock();
-    }
   }
 }
