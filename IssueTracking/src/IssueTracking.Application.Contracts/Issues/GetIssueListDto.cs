fe3896ec9eda9daea905cdd96b33833ff3ec0eee
bartvanhoey <bartvanhoey@hotmail.com>
2021-01-26 21:45:11 +0100

    Update

--- IssueTracking/src/IssueTracking.Application.Contracts/Issues/GetIssueListDto.cs
+++ IssueTracking/src/IssueTracking.Application.Contracts/Issues/GetIssueListDto.cs
@@ -1,10 +1,12 @@
 using Volo.Abp.Application.Dtos;
+using System;
 
 namespace IssueTracking.Application.Contracts.Issues
 {
   public class GetIssueListDto  : PagedAndSortedResultRequestDto
   {
     public string Filter { get; set; }
-    public bool? ShowNotActiveIssues { get; set; }
+    public bool? ShowInActiveIssues { get; set; }
+    public Guid MileStoneId{ get; set; }
   }
 }
